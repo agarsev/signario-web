c2431ebcb2cf817bbc8641ff49561b8835f8f39e
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2023-01-19 12:48:56 +0100

    Fix internal sign links
    Resolve #2

--- app/markdown.server.js
+++ app/markdown.server.js
@@ -1,5 +1,11 @@
 import { marked } from "marked";
 
+marked.use({ walkTokens: token => {
+    if (token.type === 'link' && token.href.match(/\d+/)) {
+        token.href = "/signario/signo/"+token.href;
+    }
+}});
+
 export default function markdown (text) {
     return marked.parse(text);
 }
