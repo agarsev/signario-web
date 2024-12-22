c6d091196c81e6edcd37e5a61a0470e88ca93095
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2024-12-22 18:31:34 +0100

    Add API route for signotation search

---
+++ app/routes/signario/buscar.js
@@ -0,0 +1,25 @@
+import { json } from "@remix-run/node";
+
+import { searchSN } from "../../db.server.js"; 
+
+const MAX_RESULTS = 20;
+
+export async function loader ({ request }) {
+    const params = new URL(request.url).searchParams;
+    const notation = params.get("s");
+    if (!notation) {
+        throw new Response("Bad Request", {status: 400});
+    }
+    const limit = params.get("limit") || MAX_RESULTS;
+    const signs = await searchSN(notation, limit)
+    return json({ signs: signs.map(sign_response) });
+}
+
+function sign_response ({ gloss, notation, number }) {
+    return {
+        gloss,
+        notation,
+        number,
+        video: `https://griffos.filol.ucm.es/signario/signo/${number}/video.mp4`
+    };
+}
